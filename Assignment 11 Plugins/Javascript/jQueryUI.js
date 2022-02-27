421a06e8ae7b5ab73c587677e84a79e565175db1
Hanzala Zahid <61132429+HanzalaZahid@users.noreply.github.com>
2022-02-27 14:42:06 +0500

    Assignment 11
    Plugins Practice

---
+++ Assignment 11 Plugins/Javascript/jQueryUI.js
@@ -0,0 +1,7 @@
+$(document).ready(()=>
+{
+
+    // DATE PICKER CODE HERE1
+    $('#dob').datepicker();
+
+})
---
+++ Assignment 11 Plugins/Javascript/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(()=>
+{
+    $('.slider').slick(
+        {
+            dots: true,
+            infinite: true,
+            variableWidth: true,
+            // adaptiveHeight: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            centerMode: true,
+            pauseOnHover: true,
+            // prevArrow: '<button type="button" class="slick-prev">Previous</button>'
+            
+        }
+    );
+})
