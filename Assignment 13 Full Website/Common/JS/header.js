f5c73d857ac2596713e3c6578265ec2a436889e8
Hanzala Zahid <61132429+HanzalaZahid@users.noreply.github.com>
2022-04-18 06:34:22 +0500

    Header Responsive

---
+++ Assignment 13 Full Website/Common/JS/header.js
@@ -0,0 +1,21 @@
+$(document).ready(
+    function()
+    {
+        $('header>.popup_nav>.popup_nav_container>.head>.cross>i').click(
+            function()
+            {
+                $('header>.popup_nav').css('display','none');
+                $('body>*').css('display','block');
+                $('body>script').css('display','none');
+            }
+        )
+        $('header>.top>.section_wrapper>.icons>.menu').click(
+            function()
+            {
+                $('body>*').css('display','none');
+                $('body>header').css('display','block');
+                $('header>.popup_nav').css('display','block');
+            }
+        )
+    }
+)
