186037bfdb1f3639cdc8aba1ebc9f4a1030c5ba6
Hanzala Zahid <61132429+HanzalaZahid@users.noreply.github.com>
2022-05-07 03:20:08 +0500

    ISSUE ADMIN PAGES
    NAV OPEN AND CLOSE

--- Assignment 13 Full Website/Common/JS/nav_admin.js
+++ Assignment 13 Full Website/Common/JS/nav_admin.js
@@ -3,10 +3,13 @@ $(document).ready(function(){
         $('.admin_nav .toggle_item').css("display", "flex");
         $('header>.bottom>.wrapper .menu_toggle_open').hide();
         $('header>.bottom>.wrapper .menu_toggle_close').show();
+        // $('.section_admin').animate(paddingLeft)
+        $('.section_case').addClass('section_case_nav');
       });
-    $('header>.bottom>.wrapper .menu_toggle_close').click(function(){
+      $('header>.bottom>.wrapper .menu_toggle_close').click(function(){
         $('.admin_nav .toggle_item').css("display", "none");
         $('header>.bottom>.wrapper .menu_toggle_open').show();
         $('header>.bottom>.wrapper .menu_toggle_close').hide();
+        $('.section_case').removeClass('section_case_nav');
       });
 })
